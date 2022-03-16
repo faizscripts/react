db9a03be3195c6bdd77889d8a920912b59595d66
Faiz Ahmed <4faizahmed@gmail.com>
2022-03-16 17:13:45 +0300

    Lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component{
-    constructor(props) {
-        super(props);
+    state = {lat:null, errorMessage: ''}
 
-        this.state = { lat: null, errorMessage: ''}
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({lat: position.coords.latitude})
@@ -17,13 +17,20 @@ class App extends React.Component{
         )
     }
 
-
-   render() {
+    renderContent(){
         if (this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>
         } else if (!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
-        } else return <div>Loading...</div>
+            return <SeasonDisplay lat={this.state.lat} />
+        } else return <Spinner message="Please accept location request"/>
+    }
+
+    render() {
+        return(
+            <div>
+                {this.renderContent()}
+            </div>
+        )
    }
 }
 
