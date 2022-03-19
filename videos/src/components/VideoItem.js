7bab4263a6e6719b58889bb8462acb99f5adb2ff
Faiz Ahmed <4faizahmed@gmail.com>
2022-03-19 10:53:35 +0300

    Youtube react clone

---
+++ videos/src/components/VideoItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './VideoItem.css'
+
+const VideoItem = ({video, onVideoSelect}) => {
+
+    const {thumbnails ,title} = video.snippet
+
+    return(
+        <div onClick={() => onVideoSelect(video) } className="video-item item">
+            <img className="ui image" alt="thumbnail" src={thumbnails.medium.url} />
+            <div className="content">
+                <div className="header"> {title} </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default VideoItem
