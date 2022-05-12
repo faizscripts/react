41b4b526e482ddc879f1ef3618ffe066af507de6
Faiz Ahmed <4faizahmed@gmail.com>
2022-05-12 10:50:13 +0300

    Finished revision

--- videos-hooks-test/src/components/App.js
+++ videos-hooks-test/src/components/App.js
@@ -1,50 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../api/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
-class App extends React.Component{
-    state = { videos: [], selectedVideo: null }
+const App = () => {
+    const [videos, search] = useVideos('africa')
 
-    componentDidMount() {
-        this.onTermSubmit('kenya')
-    }
 
-    onTermSubmit = async (term) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        })
+    const [selectedVideo, setSelectedVideo] = useState(null)
 
-        this.setState({
-            videos: response.data.items,
-            selectedVideo: response.data.items[0]
-        })
-    }
+    useEffect(() => {
+        setSelectedVideo(videos[0])
+    }, [videos])
 
-    onVideoSelect = (video) => {
-        this.setState({selectedVideo: video})
-    }
 
-    render() {
-        return(
-            <div className="ui container">
-                <SearchBar termSubmit={this.onTermSubmit} />
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className="five wide column">
-                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
-                        </div>
+    return(
+        <div className="ui container">
+            <SearchBar termSubmit={search} />
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                    <div className="five wide column">
+                        <VideoList onVideoSelect={(video) => setSelectedVideo(video)} videos={videos}/>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default App;
--- videos-hooks-test/src/components/SearchBar.js
+++ videos-hooks-test/src/components/SearchBar.js
@@ -1,30 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
-class SearchBar extends React.Component {
-    state = {term: ''}
+const SearchBar = ({termSubmit}) => {
+    const [term, setTerm] = useState('')
 
-    onFormSubmit = (event) => {
+    const onFormSubmit = (event) => {
         event.preventDefault()
 
-        this.props.termSubmit(this.state.term)
+        termSubmit(term)
     }
 
-    render() {
-        return (
-            <div className="search-bar ui segment">
-                    <form onSubmit={this.onFormSubmit} className="ui form">
-                        <div className="field">
-                            <label htmlFor="icon-input">
-                                Search Video
-                            </label>
-                            <div className="ui icon input">
-                                <input type="text" value={this.state.term} onChange={event => this.setState({term: event.target.value})} />
-                            </div>
-                        </div>
-                    </form>
-            </div>
-        )
-    }
+    return(
+        <div className="search-bar ui segment">
+            <form onSubmit={onFormSubmit} className="ui form">
+                <div className="field">
+                    <label htmlFor="icon-input">
+                        Search Video
+                    </label>
+                    <div className="ui icon input">
+                        <input type="text" value={term} onChange={event => setTerm(event.target.value)} />
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default SearchBar;
