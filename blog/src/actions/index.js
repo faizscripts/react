99f34d0f606406592e19bd5f821ae9c8a362a09d
Faiz Ahmed <4faizahmed@gmail.com>
2022-05-17 13:16:28 +0300

    Redux store design

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,16 +1,32 @@
+import _ from "lodash"
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPost = async () => {
-    const response = await jsonPlaceholder.get('/posts')
+export const fetchPostsAndUsers = () => {
+    return async (dispatch, getState) => {
+        await dispatch(fetchPosts())
 
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
+        // const userIds = _.uniq(_.map(getState().posts, 'userId'))
+        // userIds.forEach(id => dispatch(fetchUser(id)))
+
+        _.chain(getState().posts)
+            .map('userId')
+            .uniq()
+            .forEach(id => dispatch(fetchUser(id)))
+            .value()
     }
 }
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts')
 
-    dispatch({type: 'FETCH_POSTS', payload: response})
+    dispatch({type: 'FETCH_POSTS', payload: response.data})
+}
+
+
+export const fetchUser = (id) => {
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+
+        dispatch({type: 'FETCH_USER', payload: response.data})
+    }
 }
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,8 @@
 import {combineReducers} from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
-    replaceMe: () => 'this is a dummy key value pair to start our project before we figure out the types of reducers we will need'
+    posts: postsReducer,
+    users: usersReducer
 })
