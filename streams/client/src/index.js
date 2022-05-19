14a2d8a99808404d767a66fa8ee2b2f6e5e9ef18
Faiz Ahmed <4faizahmed@gmail.com>
2022-05-19 09:54:18 +0300

    Redux devtools

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
 const root = createRoot(document.querySelector("#root"))
-const store = createStore(reducers)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware()))
 
 root.render(
     <BrowserRouter>
